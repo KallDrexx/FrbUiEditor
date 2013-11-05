f818d6fe05b706e59aed002137b90b454f22292e
KallDrexx <me@mshapiro.net>
2013-11-04 22:45:13 -0500

    Added a dummy context menu for the tree view

--- FrbUiEditor.Core/ViewModel/UiStructureViewModel.cs
+++ FrbUiEditor.Core/ViewModel/UiStructureViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,10 +12,18 @@ namespace FrbUiEditor.Core.ViewModel
 {
     public class UiStructureViewModel : ViewModelBase
     {
+        private readonly ObservableCollection<MenuItem> _menuItems;
         private List<UiNode> _rootNode;
 
         public UiStructureViewModel()
         {
+            _menuItems = new ObservableCollection<MenuItem>(new []
+            {
+                new MenuItem {Text = "Item 1"},
+                new MenuItem {Text = "Item 2"},
+                new MenuItem {Text = "Item 3"}
+            });
+
             var reader = new XomReader();
             var nodes = reader.GenerateNodes(typeof (AssetCollection));
             var rootNode = nodes.First(x => x.IsRoot);
@@ -24,6 +33,8 @@ namespace FrbUiEditor.Core.ViewModel
                 new UiNode(rootNode, "root")
             };
         }
+
+        public IEnumerable<MenuItem> MenuItems { get { return _menuItems; } } 
 
         public List<UiNode> RootNode
         {
