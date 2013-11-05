6ad3ebc8a0c487844aab696702c01a4f759f7075
KallDrexx <me@mshapiro.net>
2013-11-04 22:29:26 -0500

    Added code to lazy load children in xml layout

--- FrbUiEditor.Core/ViewModel/UiNode.cs
+++ FrbUiEditor.Core/ViewModel/UiNode.cs
@@ -5,36 +5,71 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using GalaSoft.MvvmLight;
+using Xom.Core.Models;
 
 namespace FrbUiEditor.Core.ViewModel
 {
     public class UiNode : ViewModelBase
     {
-        private string name;
-        private readonly ObservableCollection<UiNode> children;
+        private readonly ObservableCollection<UiNode> _children;
+        private readonly XomNode _xomNode;
+        private string _name;
+        private bool _isSelected;
+        private bool _isExpanded;
+        private bool _isLoaded;
 
-        public UiNode()
+        public UiNode(XomNode node, string name)
         {
-            children = new ObservableCollection<UiNode>();
+            _children = new ObservableCollection<UiNode>();
+            _xomNode = node;
+            Name = name;
+
+            if (node.Children.Any())
+                _children.Add(new UiNode());
         }
+
+        /// <summary>
+        /// Constructor meant for creating dummy children
+        /// </summary>
+        private UiNode() {}
+
+        public IEnumerable<UiNode> Children { get { return _children; } }
 
         public string Name
         {
-            get { return name; }
-            set { Set(() => Name, ref name, value); }
+            get { return _name; }
+            set { Set(() => Name, ref _name, value); }
         }
 
-        public IEnumerable<UiNode> Children
+        public bool IsSelected
         {
-            get { return children; }
+            get { return _isSelected; }
+            set { Set(() => IsSelected, ref _isSelected, value); }
+        }
+
+        public bool IsExpanded
+        {
+            get { return _isExpanded; }
             set
             {
-                children.Clear();
-                foreach (var node in value)
-                {
-                    children.Add(node);
-                }
+                Set(() => IsExpanded, ref _isExpanded, value);
+                if (_isExpanded && !_isLoaded)
+                    LoadChildren();
             }
+        }
+
+        private void LoadChildren()
+        {
+            _children.Clear();
+
+            var childNodes = _xomNode.Children
+                                     .SelectMany(x => x.AvailableNodes)
+                                     .OrderBy(x => x.Key);
+
+            foreach (var keyValuePair in childNodes)
+                _children.Add(new UiNode(keyValuePair.Value, keyValuePair.Key));
+
+            _isLoaded = true;
         }
     }
 }
--- FrbUiEditor.Core/ViewModel/UiStructureViewModel.cs
+++ FrbUiEditor.Core/ViewModel/UiStructureViewModel.cs
@@ -3,35 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FrbUi.Xml.Models;
 using GalaSoft.MvvmLight;
+using Xom.Core;
 
 namespace FrbUiEditor.Core.ViewModel
 {
     public class UiStructureViewModel : ViewModelBase
     {
-        private List<UiNode> rootNode;
+        private List<UiNode> _rootNode;
 
         public UiStructureViewModel()
         {
+            var reader = new XomReader();
+            var nodes = reader.GenerateNodes(typeof (AssetCollection));
+            var rootNode = nodes.First(x => x.IsRoot);
+
             RootNode = new List<UiNode>
             {
-                new UiNode
-                {
-                    Name = "Root",
-                    Children = new List<UiNode>
-                    {
-                        new UiNode {Name = "Child 1"},
-                        new UiNode {Name = "Child 2"},
-                        new UiNode {Name = "Child 3"}
-                    }
-                }
+                new UiNode(rootNode, "root")
             };
         }
 
         public List<UiNode> RootNode
         {
-            get { return rootNode; }
-            set { Set(() => RootNode, ref rootNode, value); }
+            get { return _rootNode; }
+            set { Set(() => RootNode, ref _rootNode, value); }
         }
     }
 }
