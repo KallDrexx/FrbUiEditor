32690d310def2bb4486f02a21f2c58bd84ace37b
KallDrexx <me@mshapiro.net>
2013-11-08 22:12:00 -0500

    Added more friendly display name for the property description

--- FrbUiEditor.Core/Controls/NodeDetailsControl.xaml.cs
+++ FrbUiEditor.Core/Controls/NodeDetailsControl.xaml.cs
@@ -30,22 +30,35 @@ namespace FrbUiEditor.Core.Controls
         {
             var type = e.PropertyDefinition.SourceProperty.PropertyType;
 
-            var description = new StringBuilder(type.Name);
-            var genericArguments = type.GetGenericArguments();
-            for (int x = 0; x < genericArguments.Length; x++)
-            {
-                if (x == 0)
-                    description.Append("<");
-                else if (x > 0)
-                    description.Append(",");
-
-                description.Append(genericArguments[0]);
-            }
-
-            if (genericArguments.Length > 0)
-                description.Append(">");
-
+            var description = new StringBuilder();
+            AppendFriendlyTypeName(type, description);
             e.PropertyDefinition.Description = description.ToString();
+        }
+
+        private void AppendFriendlyTypeName(Type type, StringBuilder description)
+        {
+            if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Nullable<>))
+                description.Append("Nullable");
+            else if (type == typeof (Single))
+                description.Append("float");
+            else
+                description.Append(type.Name);
+
+            if (type.IsGenericType)
+            {
+                description.Append("<");
+
+                var genericArguments = type.GetGenericArguments();
+                for (int x = 0; x < genericArguments.Length; x++)
+                {
+                    if (x > 0)
+                        description.Append(",");
+
+                    AppendFriendlyTypeName(genericArguments[0], description);
+                }
+
+                description.Append(">");
+            }
         }
     }
 }
